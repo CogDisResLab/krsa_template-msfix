5fa8435767884b1253b32420dd47708540b1e4dd
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-05-04 19:39:34 -0400

    feat: add an updated pathway analysis
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- pathway_analysis.R
+++ pathway_analysis.R
@@ -1,44 +1,96 @@
-# Pathway Analysis STK - Su Data
+# Pathway Analysis
 
-library(tidyverse)
-library(enrichR)
-library(writexl)
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(enrichR)
+  library(writexl)
+})
 
-# Load the reference data
-stk_id <- readRDS("reference_data/stk_id_map.Rds")
-stk_hgnc <- readRDS("reference_data/stk_hgnc_map.Rds")
+load_reference_data <- function() {
+  load_specific_reference <- function(chip) {
+    id_file <- file.path(
+      "reference_data",
+      str_glue("{str_to_lower(chip)}_id_map.Rds")
+    )
+    hgnc_file <- file.path(
+      "reference_data",
+      str_glue("{str_to_lower(chip)}_hgnc_map.Rds")
+    )
 
-stk_map <- stk_id |>
-  inner_join(stk_hgnc)
+    id <- read_rds(id_file)
+    hgnc <- read_rds(hgnc_file)
 
-# Copy this section for each comparison
-#################
+    peptide_map <- id |>
+      inner_join(hgnc)
+  }
 
-# Change the name of the file you are loading
-sh_comparison <- read_csv("results/dpp_HSF1-SH_CTL-STK.csv") |>
-  group_by(Peptide) |>
-  filter(abs(LFC) == max(abs(LFC))) |>
-  ungroup() |>
-  select(Peptide, LFC) |>
-  inner_join(stk_map) |>
-  # Change the name of the file you are writing.
-  write_csv("results/annotated_dpp_HSF1-SH_CTL-STK.csv")
+  stk <- load_specific_reference("STK")
+  ptk <- load_specific_reference("PTK")
 
-sh_genes <- sh_comparison |>
-  select(Gene, LFC) |>
-  filter(LFC >= quantile(sh_comparison[["LFC"]], 0.90) |
-    LFC <= quantile(sh_comparison[["LFC"]], 0.10)) |>
-  pull(Gene)
+  stk |>
+    bind_rows(ptk)
+}
 
-dbs <- c(
-  "GO_Molecular_Function_2021", "GO_Cellular_Component_2021",
-  "GO_Biological_Process_2021", "KEGG_2021_Human", "Reactome_2022"
+perform_pathway_enrichment <- function(filename, peptide_map,
+                                       threshold = 0.10, databases = NULL) {
+  dpp_data <- read_csv(file.path("results", filename)) |>
+    group_by(Peptide) |>
+    filter(abs(LFC) == max(abs(LFC))) |>
+    ungroup() |>
+    select(Peptide, LFC) |>
+    inner_join(peptide_map)
+
+  lower_bound <- quantile(dpp_data[["LFC"]], threshold)
+  upper_bound <- quantile(dpp_data[["LFC"]], 1 - threshold)
+
+  selected_genes <- dpp_data |>
+    filter(LFC >= upper_bound | LFC <= lower_bound) |>
+    pull(Gene)
+
+  if (is.null(databases)) {
+    databases <- c(
+      "GO_Molecular_Function_2025",
+      "GO_Cellular_Component_2025",
+      "GO_Biological_Process_2025",
+      "KEGG_2021_Human",
+      "Reactome_Pathways_2024"
+    )
+  }
+
+  enriched <- enrichr(selected_genes, databases)
+}
+
+peptide_map <- load_reference_data()
+
+
+dpp_files <- list.files("results", "dpp") |>
+  set_names(~ .x |> str_extract("-dpp_(.*)\\.csv", 1L))
+
+dpp_names <- names(dpp_files)
+
+run_prefix <- dpp_files |>
+  basename() |>
+  str_extract("(.*)-dpp.*", 1L)
+
+outfiles <- file.path(
+  "results",
+  str_glue_data(
+    list(
+      run_prefix = run_prefix,
+      dpp_names = dpp_names
+    ),
+    "{run_prefix}-{dpp_names}-pathways.xlsx"
+  )
 )
 
-sh_enriched <- enrichr(sh_genes, dbs) |>
-  imap(~ write_csv(.x, str_glue("results/HSF1-SH_CTL-STK-{.y}-Pathways.csv")))
-
-# Change the name of the file you are writing
-write_xlsx(sh_enriched, "results/SH-STK-Pathways.xlsx")
-
-#################
+results <- dpp_files |>
+  map(
+    ~ perform_pathway_enrichment(.x, peptide_map)
+  ) |>
+  map2(
+    outfiles,
+    ~ write_xlsx(
+      .x,
+      .y
+    )
+  )
